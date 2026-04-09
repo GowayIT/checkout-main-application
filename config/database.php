2c183a2a90d04b2cb851863e4ad488103b091ffc
Goway IT <jain.joseph@goway.com>
2026-04-09 09:54:03 -0400

    Refactor Redis configuration for improved clarity

--- config/database.php
+++ config/database.php
@@ -146,37 +146,40 @@ return [
     |
     */
 
-    'redis' => [
+   'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+    'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
-            'persistent' => env('REDIS_PERSISTENT', false),
+    'options' => [
+        'cluster'    => env('REDIS_CLUSTER', 'redis'),
+        'prefix'     => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+        'persistent' => env('REDIS_PERSISTENT', false),
+        'ssl'        => [
+            'verify_peer'      => false,
+            'verify_peer_name' => false,
         ],
-
-        'default' => [
-            'scheme'   => env('REDIS_SCHEME', 'tcp'),
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-        ],
-
-        'cache' => [
-            'scheme'   => env('REDIS_SCHEME', 'tcp'),
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
-        ],
-
     ],
 
+    'default' => [
+        'scheme'   => str_starts_with(env('REDIS_HOST', ''), 'tls://') ? 'tls' : env('REDIS_SCHEME', 'tcp'),
+        'url'      => env('REDIS_URL'),
+        'host'     => str_replace('tls://', '', env('REDIS_HOST', '127.0.0.1')),
+        'username' => env('REDIS_USERNAME'),
+        'password' => env('REDIS_PASSWORD'),
+        'port'     => env('REDIS_PORT', '6379'),
+        'database' => env('REDIS_DB', '0'),
+    ],
+
+    'cache' => [
+        'scheme'   => str_starts_with(env('REDIS_HOST', ''), 'tls://') ? 'tls' : env('REDIS_SCHEME', 'tcp'),
+        'url'      => env('REDIS_URL'),
+        'host'     => str_replace('tls://', '', env('REDIS_HOST', '127.0.0.1')),
+        'username' => env('REDIS_USERNAME'),
+        'password' => env('REDIS_PASSWORD'),
+        'port'     => env('REDIS_PORT', '6379'),
+        'database' => env('REDIS_CACHE_DB', '1'),
+    ],
+
+],
 
 ];
